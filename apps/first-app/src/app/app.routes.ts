24d18f3b3b42995a5e556b779c941be6a3adfb07
mohammad younes <my.mobalegh@demisco.com>
2024-11-04 17:10:44 +0330

    app routes

--- apps/first-app/src/app/app.routes.ts
+++ apps/first-app/src/app/app.routes.ts
@@ -4,6 +4,23 @@ import { SharedComponent } from '@shared';
 export const appRoutes: Route[] = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    // pathMatch: 'full',
     component: SharedComponent,
   },
+  {
+    path: 'about',
+    pathMatch: 'full',
+    component: SharedComponent,
+  },
+  // {
+  //   path: '**',
+  //   // pathMatch: 'full',
+  //   // component: SharedComponent,
+  //   redirectTo: 'home'
+  // },
 ];
